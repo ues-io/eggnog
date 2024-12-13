064033e62cb2596bec7b94608dc1f7760df2330e
Gregg Baxter <gregg@ues.io>
2024-12-13 15:01:38 +0100

    update-success-url

--- generated/@types/@uesio/app.d.ts
+++ generated/@types/@uesio/app.d.ts
@@ -14,8 +14,8 @@ declare module "@uesio/app/bots/listener/eurocanuck/eggnog/pay_with_stripe" {
 	type Params = {
 		order: string
 		currency?: string
-		successUrl?: string
-		cancelUrl?: string
+		successUrl: string
+		cancelUrl: string
 	}
 
 	export type {
